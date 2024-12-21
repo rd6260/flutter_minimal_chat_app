badbed3a3757fdb31fd86a85eb3b60818ebf573c
rd <rohandas.zero@gmail.com>
2024-12-21 23:41:26 +0530

    initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_minimal_chat_app/widgets/custom_button.dart';
+import 'package:flutter_minimal_chat_app/widgets/custom_textfield.dart';
+
+class LoginScreen extends StatelessWidget {
+  // Text Field controllers
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginScreen({super.key});
+
+  void loginFunction() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.chat,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 50),
+
+            // Welcome message
+            Text(
+              "Welcome! Lets login in",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 25),
+            // email
+            CustomTextfield(
+              hintText: "Phone Number",
+              controller: _phoneNumberController,
+            ),
+            const SizedBox(height: 10),
+            // password
+            CustomTextfield(
+              hintText: "Password",
+              isPassword: true,
+              controller: _passwordController,
+            ),
+            const SizedBox(height: 10),
+            // login
+            CustomButton(
+              text: "Login",
+              onTap: loginFunction,
+            ),
+            const SizedBox(height: 10),
+            // register
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account? ",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                Text(
+                  "Register now!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomTextfield extends StatelessWidget {
+  final String hintText;
+  final bool isPassword;
+  final TextEditingController controller;
+  const CustomTextfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: TextField(
+        controller: controller,
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Theme.of(context).colorScheme.secondary,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.tertiary,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
