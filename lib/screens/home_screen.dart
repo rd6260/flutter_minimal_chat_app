19003776fc5404b03e6e3af8009e6f5a737d0219
rd <rohandas.zero@gmail.com>
2024-12-26 16:09:10 +0530

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void logoutFunction() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello; World!"),
+      appBar: AppBar(
+        title: Text("Chats"),
+        actions: [
+          // logout button
+          IconButton(
+            onPressed: logoutFunction,
+            icon: Icon(
+              Icons.logout,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
